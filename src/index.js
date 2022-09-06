e3586603b12627cbeba174f547eee140331da808
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-09-06 11:00:24 +0200

    Update branch.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
 import App from './App';
 import './index.css';
 
@@ -10,8 +9,8 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
