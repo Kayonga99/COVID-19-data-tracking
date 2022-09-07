e511c5d5f87577006026219b94c9a61744454b1e
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-09-07 16:00:15 +0200

    Update branch.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { fetchData } from '../redux/covidData/covid';
 
 function Home() {
   const covidReducer = useSelector((state) => state.covidReducer.covid);
   const dispatch = useDispatch();
+  let Europe = covidReducer.filter((item) => item.continent === 'Europe');
+  const loci = useLocation();
+  const query = new URLSearchParams(loci.search);
+  const buscar = query.get('buscar') || '';
+
+  const navigate = useNavigate();
+  Europe = Europe.filter((country) => country.country.includes(buscar.toLocaleUpperCase()));
+  const [searchInput, setSearchInput] = useState(buscar);
+
   useEffect(() => {
     if (!covidReducer.length) {
       dispatch(fetchData());
     }
   }, []);
-  const Europe = covidReducer.filter((item) => item.continent === 'Europe');
+
+  const filterCountry = (e) => {
+    navigate(e.target.value ? `?buscar=${e.target.value}` : '');
+    setSearchInput(e.target.value);
+  };
   return (
     <>
       <div className="heading">
@@ -19,8 +32,10 @@ function Home() {
         <h1>EUROPE COUNTRIES&#39; COVID-19 DATA</h1>
         {' '}
       </div>
+      <div className="input-box">
+        <input value={searchInput} onChange={filterCountry} className="input" type="text" placeholder="Serch European Country ..." />
+      </div>
       <div className="body">
-
         {
         Europe.map((data) => {
           const {
@@ -32,8 +47,10 @@ function Home() {
             <Link key={country} to={{ pathname: `/country/${country}` }}>
 
               <div className="main-cont" key={country}>
-                {country}
-                <img className="img" src={flag} alt={country} />
+                <h2 className="country">{country}</h2>
+                <div className="image">
+                  <img className="img" src={flag} alt={country} />
+                </div>
 
               </div>
             </Link>
