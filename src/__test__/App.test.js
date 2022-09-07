9c4aa499d9ebac31005480d515b8447653214e78
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-09-07 22:53:14 +0200

    Fix linter errors.

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
-import store from '../Redux/configureStore';
+import store from '../redux/configureStore';
 import App from '../App';
 
 describe('App component', () => {
